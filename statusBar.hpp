f0329251c7af2502c2cb890e88df9f539ddf95eb
DarthRogue <mdswish@gmail.com>
2015-04-13 09:03:18 -0500

    Status Bar with Icons
    Initial Commit

---
+++ statusBar.hpp
@@ -0,0 +1,67 @@
+#define ST_RIGHT 0x01
+ 
+class osefStatusBarAdmin {
+	idd = -1;
+	onLoad = "uiNamespace setVariable ['osefStatusBarAdmin', _this select 0]";
+	onUnload = "uiNamespace setVariable ['osefStatusBarAdmin', objNull]";
+	onDestroy = "uiNamespace setVariable ['osefStatusBarAdmin', objNull]";
+	fadein = 0;
+	fadeout = 0;
+	duration = 10e10;
+	movingEnable = 0;
+	controlsBackground[] = {};
+	objects[] = {};
+	class controls {
+		class statusBarText {
+			idc = 55554;
+			x = safezoneX + safezoneW - 2;
+			y = safezoneY + safezoneH - 0.1;
+			w = 1.6;
+			h = 0.07;
+			shadow = 2;
+			colorBackground[] = { 0, 0, 0, 0.5 };  // uncomment and increase 4th number to have a background
+			font = "PuristaSemibold";
+			size = 0.04;
+			type = 13;
+			style = 2;
+			text="";
+			class Attributes {
+				align="left";
+				color = "#ffffff";//#5fe60c
+			};
+		};
+	};
+}; 
+
+class osefStatusBar {
+	idd = -1;
+	onLoad = "uiNamespace setVariable ['osefStatusBar', _this select 0]";
+	onUnload = "uiNamespace setVariable ['osefStatusBar', objNull]";
+	onDestroy = "uiNamespace setVariable ['osefStatusBar', objNull]";
+	fadein = 0;
+	fadeout = 0;
+	duration = 10e10;
+	movingEnable = 0;
+	controlsBackground[] = {};
+	objects[] = {};
+	class controls {
+		class statusBarText {
+			idc = 55555;
+			x = safezoneX + safezoneW - 1.85;
+			y = safezoneY + safezoneH - 0.1;
+			w = 1.3;
+			h = 0.07;
+			shadow = 2;
+			colorBackground[] = { 0, 0, 0, 0.5 };  // uncomment and increase 4th number to have a background
+			font = "PuristaSemibold";
+			size = 0.04;
+			type = 13;
+			style = 2;
+			text="";
+			class Attributes {
+				align="left";
+				color = "#ffffff";//#5fe60c
+			};
+		};
+	};
+}; 
